6fc5099e74e96a374da814e777548d3e45b48569
Pratham Batra <pratham11.batra@gmail.com>
2025-07-24 22:24:17 +0530

    refactor: change proxy api base url from localhost to deployed render url

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -1,2 +1,2 @@
-const PROXY_API_BASE_URL = "http://localhost:3001/api";
+const PROXY_API_BASE_URL = "https://cineverse-reverse-proxy.onrender.com/api";
 const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
